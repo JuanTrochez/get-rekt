6f2bb3488455963c1ffb11e0580409df1f8f3a42
Juan <juan.trochez@hotmail.fr>
2016-07-25 11:35:32 +0200

    category dao

---
+++ get-rekt/Dao/category/CategoryDaoDb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using get_rekt.Models;
+
+namespace get_rekt.Dao.category
+{
+    public class CategoryDaoDb : ICategoryDao
+    {
+        public CategoryDaoDb() { }
+        public CategoryModel getCategory(int id)
+        {
+            Context contextCategory = new Context();
+
+            CategoryModel category = contextCategory.Categories
+                .Where(c => c.Id == id)
+                .FirstOrDefault();
+
+            return category;
+        }
+    }
+}
---
+++ get-rekt/Dao/category/ICategoryDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using get_rekt.Models;
+
+namespace get_rekt.Dao.category
+{
+    interface ICategoryDao
+    {
+        CategoryModel getCategory(int id);
+    }
+}
